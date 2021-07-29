8d24f20939331d789679113290639fa0b61b9aae
Sergey Fomenko <fomenko.personal@gmail.com>
2021-07-29 09:14:42 +0200

    refactor: refactor cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,6 +21,20 @@ function Main(props) {
       })
   }, []);
 
+  const cardsList = cards.map((card, index) => (
+    <article className="card" key={index}>
+      <img className="card__image" src={card.link} alt={card.name}/>
+      <div className="card__inner">
+        <h2 className="card__title">{card.name}</h2>
+        <div className="card__wrapper">
+          <button className="card__button card__button_type_like" type="button"/>
+          <span className="card__likes-count">{card.likes.length}</span>
+        </div>
+      </div>
+      <button className="card__button card__button_type_remove" type="button"/>
+    </article>
+  ));
+
   return (
     <main className="content">
       <section className="profile">
@@ -37,21 +51,7 @@ function Main(props) {
         </div>
         <button className="profile__button profile__button_type_add" type="button" onClick={props.onAddPlace}/>
       </section>
-      <section className="elements">
-        {cards.map((card, index) => (
-          <article className="card" key={index}>
-            <img className="card__image" src={card.link} alt={card.name}/>
-            <div className="card__inner">
-              <h2 className="card__title">{card.name}</h2>
-              <div className="card__wrapper">
-                <button className="card__button card__button_type_like" type="button"/>
-                <span className="card__likes-count">{card.likes.length}</span>
-              </div>
-            </div>
-            <button className="card__button card__button_type_remove" type="button"/>
-          </article>
-        ))}
-      </section>
+      <section className="elements">{cardsList}</section>
     </main>
   );
 }
